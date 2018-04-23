d0ebeb65db20b54b03a1984d6814a860608acd11
brickett <brickett@users.noreply.github.com>
2018-04-23 18:01:43 -0500

    Import and initial cleaning

--- WageGrowth.R
+++ WageGrowth.R
@@ -1,1 +1,17 @@
 ### Wage Growth Calculated from CPS Data ###
+
+## Setup ##
+library(haven)
+
+# set directory to GitHub repo
+setwd("C:/Users/bjr21/Documents/GitHub/WageGrowthCPS")
+
+## Import Dataset ##
+wages <- read_sas("cps_00020.sas7bdat")
+
+# Screen for those with eligible employment data
+wages <- subset(wages, wages$ELIGORG == 1)
+wages <- subset(wages, wages$EARNWEEK != 9999.99 | wages$HOURWAGE != 99.99) #cleans out data points where both hourly and weekly wages are not known
+
+
+
